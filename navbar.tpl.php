b87ec1f0b0cb47cac5aaecbb686a9f318acdc238
J. Renée Beach <splendidnoise@gmail.com>
2012-09-19 19:08:56 -0400

    Porting D8 Toolbar work back.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- navbar.tpl.php
+++ navbar.tpl.php
@@ -15,29 +15,40 @@
  *
  * @see template_preprocess()
  * @see template_preprocess_navbar()
+ *
+ * @ingroup themeable
  */
 ?>
-<nav id="navbar" role="navigation" class="<?php print $classes; ?> clearfix" <?php print $attributes; ?>>
-  <div class="navbar-bar">
-    <div class="navbar-menu clearfix">
-      <?php print render($navbar['navbar_tray_toggle']); ?>
-      <?php print render($navbar['navbar_home']); ?>
-      <?php print render($navbar['navbar_user']); ?>
-      <?php //print render($navbar['navbar_menu']); ?>
-      <?php if ($navbar['navbar_drawer']):?>
-        <?php print render($navbar['navbar_toggle']); ?>
+<div id="navbar" role="navigation" class="<?php print $attributes['class']; ?> clearfix" <?php print $attributes; ?>>
+  <div class="navbar-bar clearfix">
+    <div class="section first">
+      <?php print render($navbar['navbar_navigation']); ?>
+    </div>
+    <div class="section second">
+      <?php if (!empty($navbar['action_links'])): ?>
+        <h2 class="element-invisible"><?php print t('navbar page actions'); ?></h2>
+          <?php print render($navbar['action_links']); ?>
       <?php endif; ?>
     </div>
-    <?php print render($navbar['navbar_drawer']); ?>
+    <div class="section third">
+      <?php if (!empty($navbar['navbar_user'])) : ?>
+        <?php print render($navbar['navbar_user']); ?>
+      <?php endif; ?>
+    </div>
   </div>
 
-  <div class="navbar-tray">
-    <div class="filter-search clearfix">
+  <div class="navbar-tray" name="navbar-tray">
+    <div class="lining slider">
       <?php print render($navbar['navbar_filter']); ?>
-      <span class="close"><?php print t('x'); ?></span>
-    </div>
-    <div class="tray-menu clearfix">
-      <?php print render($navbar['navbar_menu']); ?>
+      <?php if (!empty($navbar['navbar_shortcuts'])) : ?>
+        <?php print render($navbar['navbar_shortcuts']); ?>
+      <?php endif; ?>
+      <?php if (!empty($navbar['navbar_menu'])) : ?>
+        <nav class="navbar-menu">
+          <h2 class="element-invisible"><?php print t('Administration menu'); ?></h2>
+          <?php print render($navbar['navbar_menu']); ?>
+        </nav>
+      <?php endif; ?>
     </div>
   </div>
-</nav>
+</div>
